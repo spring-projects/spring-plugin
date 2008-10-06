4bd432afd0f6c5a7062972611712239002501cfa
Oliver Gierke <info@olivergierke.de>
2008-10-06 20:18:54 +0000

    * added unit test from minos-core
    git-svn-id: svn+ssh://svn.synyx.de/var/svn/synyx/opensource/synyx-plugin/trunk@2913 5a64d73e-33d6-4ccc-9058-23f8668ecac9

---
+++ plugin-core/src/test/java/org/synyx/plugin/core/PluginRegistryUnitTest.java
@@ -0,0 +1,48 @@
+package org.synyx.plugin.core;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class PluginRegistryUnitTest {
+
+    private SamplePlugin provider;
+
+    private PluginRegistry<SamplePlugin, String> registry;
+
+
+    /**
+     * Initializes a {@code PluginRegistry} and equips it with an {@code
+     * EmailNotificationProvider}.
+     */
+    @Before
+    public void setUp() {
+
+        provider = new SamplePluginImplementation();
+
+        registry = new PluginRegistry<SamplePlugin, String>();
+        registry.setPlugins(Arrays.asList(provider));
+    }
+
+
+    /**
+     * Asserts asking for a plugin with the {@code PluginMetadata} provided by
+     * the {@link EmailNotificationProvider}.
+     */
+    @Test
+    public void assertFindsEmailNotificationProvider() {
+
+        String metadata = "FOO";
+
+        List<SamplePlugin> plugins = registry.getPluginsFor(metadata);
+        Assert.assertNotNull(plugins);
+        Assert.assertEquals(1, plugins.size());
+
+        SamplePlugin provider = plugins.get(0);
+        Assert.assertTrue(provider instanceof SamplePluginImplementation);
+    }
+}
