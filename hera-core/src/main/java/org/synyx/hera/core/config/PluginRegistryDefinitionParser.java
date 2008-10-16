36952dbce0584a5eb1accd2988ca8e8e0ec6e5dd
Oliver Gierke <info@olivergierke.de>
2008-10-16 06:33:43 +0000

    * added Apache Licence 2.0 text  * fixed #2
    git-svn-id: svn+ssh://svn.synyx.de/var/svn/synyx/opensource/hera/trunk@3039 5a64d73e-33d6-4ccc-9058-23f8668ecac9

--- hera-core/src/main/java/org/synyx/hera/core/config/PluginRegistryDefinitionParser.java
+++ hera-core/src/main/java/org/synyx/hera/core/config/PluginRegistryDefinitionParser.java
@@ -1,9 +1,27 @@
+/*
+ * Copyright 2002-2008 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package org.synyx.hera.core.config;
 
 import org.w3c.dom.Element;
 
 
 /**
+ * Simple extension of {@link PluginListDefinitionParser}. Simply registers a
+ * {@code PluginListDefinitionParser} instead of the original class.
+ * 
  * @author Oliver Gierke - gierke@synyx.de
  */
 public class PluginRegistryDefinitionParser extends PluginListDefinitionParser {
